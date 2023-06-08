1912ed14ef3e2a81a9454cc1c6a5cf007275e75c
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-06-09 00:01:31 +0530

    Started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const getData = async () => {
+    try {
+      const response = await axios.get("/api/posts");
+      const {
+        status,
+        data: { posts },
+      } = response;
+      if (status === 200) {
+        setPosts(posts);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(posts);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Social Media Application</h1>
+      {/* {posts} */}
     </div>
   );
 }
